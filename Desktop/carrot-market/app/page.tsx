985aea00f01939b326305a059d44cea3d66f7c69
taegeumLee <xorma443101@gmail.com>
2024-09-12 11:31:52 +0900

    #3.4[TAILWIND] Variables

--- Desktop/carrot-market/app/page.tsx
+++ Desktop/carrot-market/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600">
+    <main className="bg-gray-100 h-screen flex items-center justify-center flex-col p-5 dark:bg-gray-700">
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm mb-5 dark:bg-gray-600">
         <div className="flex justify-between items-center">
           <div className="flex flex-col">
             <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
@@ -34,6 +34,17 @@ export default function Home() {
           <span>In transit</span>
           <span className="text-gray-400 dark:text-gray-500">Delivered</span>
         </div>
+      </div>
+
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 ">
+        <input
+          className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-orange-300 ring-offset-2 focus:ring-offset-blue-300 ring-transparent placeholder:drop-shadow-md"
+          type="text"
+          placeholder="Search here..."
+        />
+        <button className="bg-black bg-opacity-40  text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none">
+          Search
+        </button>
       </div>
     </main>
   );
