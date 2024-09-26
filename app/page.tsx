f8da2d8b1da7a5106016433f0b7eead19316ae11
taegeumLee <xorma443101@gmail.com>
2024-09-26 13:34:39 +0900

    4.1 Create Account Screen

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,20 @@ export default function Home() {
     <div className="flex flex-col items-center justify-between min-h-screen p-6">
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium ">
         <span className="text-9xl ">🥕</span>
-        <h1 className="text-4xl">당근</h1>
-        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
+        <h1 className="text-4xl">Carrot</h1>
+        <h2 className="text-2xl">Welcome to Carrot Market!</h2>
       </div>
       <div className="flex flex-col items-center gap-3 w-full">
         <Link
           href="/create-account"
-          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-600 transition-colors"
+          className="primary-button py-2.5 text-lg text-white"
         >
-          시작하기
+          Join Us
         </Link>
-        <div className="flex gap-2">
-          <span>이미 계정이 있나요?</span>
+        <div className="flex gap-2 ">
+          <span>Already have an account?</span>
           <Link href="/login" className="hover:underline">
-            로그인
+            Log In
           </Link>
         </div>
       </div>
