873b65080faea84aeff7b801d2d4315e9ea2d3fa
taegeumLee <xorma443101@gmail.com>
2024-09-30 11:11:58 +0900

    #6.2 Refinement

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,12 +1,39 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(4).max(15),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirmPassword: z.string().min(10),
-});
+function validateUsername(username: string) {
+  return username.includes("yami");
+}
+
+const checkPassword = ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => {
+  password === confirmPassword;
+};
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "username must be a string",
+      })
+      .min(4, "way too short")
+      .max(15, "way too long")
+      .refine(
+        (username) => validateUsername(username),
+        "username must be unique"
+      ),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirmPassword: z.string().min(10),
+  })
+  .refine(({ password, confirmPassword }) => checkPassword, {
+    message: "passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
