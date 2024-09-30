6d4914c55d7c413622566a6893dda79c12fe662a
taegeumLee <xorma443101@gmail.com>
2024-09-30 11:21:44 +0900

    #6.3 Transformation

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,6 +1,10 @@
 "use server";
 import { z } from "zod";
 
+const passwordRegex = new RegExp(
+  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+);
+
 function validateUsername(username: string) {
   return username.includes("yami");
 }
@@ -22,13 +26,21 @@ const formSchema = z
       })
       .min(4, "way too short")
       .max(15, "way too long")
+      .trim()
+      .toLowerCase().transform(username => )
       .refine(
         (username) => validateUsername(username),
         "username must be unique"
       ),
-    email: z.string().email(),
-    password: z.string().min(10),
-    confirmPassword: z.string().min(10),
+    email: z.string().email().toLowerCase(),
+    password: z
+      .string()
+      .min(10)
+      .regex(
+        passwordRegex,
+        "A password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+      ),
+    confirmPassword: z.string(),
   })
   .refine(({ password, confirmPassword }) => checkPassword, {
     message: "passwords do not match",
@@ -45,5 +57,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   const result = formSchema.safeParse(data);
   if (!result.success) {
     return result.error.flatten();
+  } else {
+    console.log(result.data);
   }
 }
