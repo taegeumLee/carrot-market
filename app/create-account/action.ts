3e0c3e74ffa983e01fe36fd8d97d8e946494e79b
taegeumLee <xorma443101@gmail.com>
2024-10-02 23:33:28 +0900

    #8.1 Database Validation

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -4,11 +4,35 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
 
 function validateUsername(username: string) {
   return username.includes("yami");
 }
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username: username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const userEmail = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(userEmail);
+};
 
 const checkPassword = ({
   password,
@@ -30,8 +54,13 @@ const formSchema = z
       .refine(
         (username) => validateUsername(username),
         "username must be unique"
-      ),
-    email: z.string().email().toLowerCase(),
+      )
+      .refine(checkUniqueUsername, "This username is already taken"),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .refine(checkUniqueEmail, "There is already an account with this email"),
     password: z
       .string()
       .min(PASSWORD_MIN_LENGTH)
@@ -50,10 +79,13 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirmPassword: formData.get("confirm-password"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    //hash the password
+    //save the user to the database
+    //log the user in
+    //redirect to the '/'
   }
 }
